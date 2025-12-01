98afb224b3bf37ed646c4eae56c143a25f418b97
luizjardim <luizmj@gmail.com>
2025-12-01 12:58:27 +0000

    feat: add scripts `lint:check` and `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-luiz-tabnews
+
 Um clone do tabnews.com.br como ferramenta educacional e aprendizagem
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,7 @@
-function Home(){
-    return <h1>Modi Eu Te AMOOOO S2</h1>
+function Home() {
+  return <h1>Modi Eu Te AMOOOO S2</h1>;
 }
-export default Home; //o default significa o padrao que a partir dele chama as outras 
+function Test() {
+  console.log("teste");
+}
+export default Home; //o default significa o padrao que a partir dele chama as outras

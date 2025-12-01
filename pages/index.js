9e24ae05042ec0cfdfa3b328cec18c016d879a32
luizjardim <luizmj@gmail.com>
2025-12-01 23:06:53 +0000

    style: aplicando lint:fix

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,4 @@
 function Home() {
   return <h1>Modi Eu Te AMOOOO S2</h1>;
 }
-function Test() {
-  console.log("teste");
-}
-export default Home; //o default significa o padrao que a partir dele chama as outras
+export default Home;

3a9d415a1b0a24622b0887b31f07e7840a941324
luizjardim <luizmj@gmail.com>
2026-03-30 12:43:04 -0300

    style: lint prettier fix

--- tests/integrations/api/v1/migrations/get.test.js
+++ tests/integrations/api/v1/migrations/get.test.js
@@ -4,15 +4,15 @@ beforeAll(async () => {
   await orquestrator.waitForAllServices();
   await database.query("drop schema public cascade; create schema public;");
 });
-describe("GET to api/v1/migrations", ()=> {
-describe("Anonymous user",()=>{
-test("Running pending migrations", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations");
-  expect(response.status).toBe(200);
-  const responseBody = await response.json();
-  expect(Array.isArray(responseBody)).toBe(true);
-  expect(process.env.NODE_ENV === "test").toBe(true);
-  expect(responseBody.length).toBeGreaterThan(0);
+describe("GET to api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    test("Running pending migrations", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations");
+      expect(response.status).toBe(200);
+      const responseBody = await response.json();
+      expect(Array.isArray(responseBody)).toBe(true);
+      expect(process.env.NODE_ENV === "test").toBe(true);
+      expect(responseBody.length).toBeGreaterThan(0);
+    });
+  });
 });
-})
-})
